0ebfb16affff9dc2b7ab387de86fcb46f7a5f74d
HYUNMINKIM <khyunm91@gmail.com>
2021-05-02 22:50:54 +0900

    자동차 객체 생성 자동차 생성하는 로직

---
+++ src/main/java/RacingCar.java
@@ -0,0 +1,31 @@
+import vo.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class RacingCar {
+
+    private static Scanner scanner = new Scanner(System.in);
+
+    public List<Car> createCarList() {
+        String cars = scanner.next();
+        String[] carArray = cars.split(",");
+        List<Car> carList = new ArrayList<>();
+
+        for(String carName : carArray) {
+            carList.add(createCar(carName));
+        }
+        carList.removeIf(n -> n.getCarName().length() == 0);
+        return carList;
+
+    }
+
+    public Car createCar(String carName) {
+        return Car.builder()
+                .carName(carName)
+                .distance(0)
+                .build();
+    }
+
+}
---
+++ src/main/java/vo/Car.java
@@ -0,0 +1,17 @@
+package vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Builder
+@AllArgsConstructor
+public class Car {
+
+    String carName;
+    int distance;
+
+}
